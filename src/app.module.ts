32459d33d0a3d8c0728dc2143197eb9555786f48
MatviieshynO <Olopromotion@gmail.com>
2023-12-27 09:19:43 +0100

    feat(config): install and configure ConfigModule, create .env and .env.example files
    - Installed ConfigModule for managing configuration in the application.
    - Configured the module with necessary settings for environment-specific variables.
    - Created a .env file for storing environment-specific configuration.
    - Created a .env.example file to provide an example of environment variables.
    
    #BREAKING CHANGE: None

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      envFilePath: ['.env'],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  const API_PORT: number = configService.get<number>('API_PORT') || 3000;
+  const API_HOST: string = configService.get<string>('API_HOST') || '127.0.0.1';
+
+  await app.listen(API_PORT, API_HOST);
 }
 bootstrap();
